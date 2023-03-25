1f02a6cb8ea0b71491b3c6c7abfdb30b7cc14b3c
Willian Camacho <wcamacho221@gmail.com>
2023-03-25 01:46:54 -0500

    Aplicativo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import ImprimirFecha from './components/ImprimirFecha.vue'
+import Eliminar from './components/Eliminar.vue';
+
 </script>
 
 <template>
@@ -8,31 +10,39 @@ import ImprimirFecha from './components/ImprimirFecha.vue'
   </header>
 
   <div>
+     <!-- Seria los inputs  -->
     <input type="number" v-model="imputdia" placeholder="dia" />
     <input type="number" v-model="imputmes" placeholder="mes" />
     <input type="number" v-model="imputaño" placeholder="año" />
   </div>
   <div>
-    <button @click="esactivo">enviar</button>
-  </div>
-  <div v-if="activo">
+    <!-- Seria el componente hijo que imprime la fecha -->
     <ImprimirFecha :dia="imputdia" :mes="imputmes" :año="imputaño" />
   </div>
+
+  <!-- Seria el componente hijo que elimina los datos de la fecha       Serian funciones para actualizar los datos de los props -->
+  <Eliminar :dia="imputdia" :mes="imputmes" :año="imputaño" @delete-fecha = "changeValue"/>
+
 </template>
+
 <script>
+
+// Seria para exportar los datos para ser usados por los hijos
 export default {
   data() {
     return {
       imputdia: '',
       imputmes: '',
-      imputaño: '',
-      activo: false
+      imputaño: ''   
     }
   },
-  methods: {
-    esactivo() {
-      this.activo = true
-    }
+  // Seria los metodos usados para actualizar los datos de las props 
+  methods:{
+    changeValue(dia,mes,ano){
+      this.imputdia = dia
+      this.imputmes = mes
+      this.imputaño = ano
+    },
   }
 }
 </script>
--- src/components/ImprimirFecha.vue
+++ src/components/ImprimirFecha.vue
@@ -17,8 +17,48 @@ defineProps({
 
 <template>
   <div>
-    <p>{{ dia }}/{{ mes }}/{{ año }}</p>
+    <!-- Se imprime la fecha -->
+    <button @click="esactivo">enviar</button>
+    <p>{{ diaD }}/{{ mesM }}/{{ añoA }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      diaD: 'DD',
+      mesM: 'MM',
+      añoA: 'AAAA',
+      isActive: false
+    }
+  },
+  methods: {
+    esactivo() {
+      this.isActive = true; // Bandera para saber si el boton fue presionado
+      this.diaD = this.$props.dia;
+      this.mesM = this.$props.mes;
+      this.añoA = this.$props.año;
+    }
+  },
+  watch:{
+    dia: function(newValue, valueBefore){
+      if (this.isActive === true){
+        this.diaD = newValue
+      }
+    },
+    mes:function(newValue,valueBefore){
+      if(this.isActive === true){
+        this.mesM = newValue
+      }
+    },
+    año: function(newValue,valueBefore){
+      if (this.isActive === true){
+        this.añoA = newValue
+      }
+    }
+  }
+}
+</script>
 
 <style scoped></style>
