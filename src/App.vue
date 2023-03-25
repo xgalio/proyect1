ba0241a5f7563e6c003886811dcefe55cf381dc9
xgalio <xgalio18@gmail.com>
2023-03-25 00:16:44 -0500

    primera version

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import ImprimirFecha from './components/ImprimirFecha.vue'
+</script>
+
+<template>
+  <header>
+    <h1>aplicacion fecha</h1>
+  </header>
+
+  <div>
+    <input type="number" v-model="imputdia" placeholder="dia" />
+    <input type="number" v-model="imputmes" placeholder="mes" />
+    <input type="number" v-model="imputaño" placeholder="año" />
+  </div>
+  <div>
+    <button @click="esactivo">enviar</button>
+  </div>
+  <div v-if="activo">
+    <ImprimirFecha :dia="imputdia" :mes="imputmes" :año="imputaño" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      imputdia: '',
+      imputmes: '',
+      imputaño: '',
+      activo: false
+    }
+  },
+  methods: {
+    esactivo() {
+      this.activo = true
+    }
+  }
+}
+</script>
+<style scoped></style>
---
+++ src/components/ImprimirFecha.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  dia: {
+    type: Number,
+    required: true
+  },
+  mes: {
+    type: Number,
+    required: true
+  },
+  año: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ dia }}/{{ mes }}/{{ año }}</p>
+  </div>
+</template>
+
+<style scoped></style>
